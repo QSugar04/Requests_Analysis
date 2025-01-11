86363fcf4d32ca4ae5d3b820474dcff3996b6373
xtl <xtlsoft@dlut.com>
2025-01-11 12:22:59 +0800

    data

---
+++ data.py
@@ -0,0 +1,15 @@
+from github import Github
+# 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
+# 使用个人的GitHub Token进行身份验证
+token = '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'
+g = Github(token)
+
+# 获取PEP仓库
+repo = g.get_repo("psf/requests")
+
+# 获取仓库的提交历史
+commits = repo.get_commits()
+
+# 打印提交信息示例
+for commit in commits:
+    print(commit.commit.author.date, commit.commit.author.name, commit.commit.message)
