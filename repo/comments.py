7d80f292a3b90adfd7321bd01e6c12807348ce09
wyh-1115 <3072286369@qq.com>
2025-01-22 14:38:22 +0800

    Update comments.py

--- repo/comments.py
+++ repo/comments.py
@@ -8,4 +8,15 @@ comments = repo.get_comments()
 # 统计评论的作者
 comment_authors = Counter(comment.user.login for comment in comments)
 
+# 绘制评论数量最多的前N位作者
+top_n = 15  # 选择你希望的前N位作者数量
+top_comment_authors = dict(comment_authors.most_common(top_n))
+other_comment_authors = dict(comment_authors - Counter(top_comment_authors))
 
+comment_labels = list(top_comment_authors.keys()) + ['Other']
+comment_sizes = list(top_comment_authors.values()) + [sum(other_comment_authors.values())]
+
+plt.pie(comment_sizes, labels=comment_labels, autopct='%1.1f%%', startangle=140)
+plt.axis('equal')
+plt.title(f'Top {top_n} Comment Authors and Others')
+plt.savefig(f'Top {top_n} Comment Authors and Others')
