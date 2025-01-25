f209bef3e49d19235a4706a220d77cb81e2b2025
CAVUZY <1402197694@qq.com>
2025-01-25 22:02:02 +0800

    Update count.py

--- Submit_History/count.py
+++ Submit_History/count.py
@@ -8,8 +8,8 @@ commit_month_count = Counter(date.strftime('%Y-%m') for date in commit_dates)
 
 # 绘制提交数量随着时间的变化图表并保存
 months, counts = zip(*sorted(commit_month_count.items()))  # 确保按时间排序
-plt.figure(figsize=(10, 6))  # 设置图像尺寸
-plt.plot(months, counts, marker='o')  # 添加数据点标记
+plt.figure(figsize=(10, 6))  
+plt.plot(months, counts, marker='o') 
 plt.xlabel('Month')
 plt.ylabel('Number of Commits')
 plt.title('Requests Repository Commit Evolution')
