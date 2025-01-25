a90d3978fc040e0c1b148950c0fa0c36c5ef1372
CAVUZY <1402197694@qq.com>
2025-01-25 22:04:47 +0800

    Update data.py

--- time/data.py
+++ time/data.py
@@ -1,8 +1,11 @@
 from github import Github
+
+# 使用个人的GitHub Token进行身份验证
 token='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'
+
 g = Github(token)
 
-# 获取  仓库
+# 获取仓库
 repo = g.get_repo("psf/requests")
 
 # 获取仓库的提交历史
