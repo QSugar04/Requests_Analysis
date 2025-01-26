040f427be9a1c64b06d9c66ac329e01ccc3efaa8
yuanshen581 <1044506869@qq.com>
2025-01-26 22:16:21 +0800

    Update comments.py

--- repo/comments.py
+++ repo/comments.py
@@ -16,7 +16,14 @@ other_comment_authors = dict(comment_authors - Counter(top_comment_authors))
 comment_labels = list(top_comment_authors.keys()) + ['Other']
 comment_sizes = list(top_comment_authors.values()) + [sum(other_comment_authors.values())]
 
+# 使用matplotlib的pie函数绘制饼图
 plt.pie(comment_sizes, labels=comment_labels, autopct='%1.1f%%', startangle=140)
+
+# 设置饼图为正圆形
 plt.axis('equal')
+
+# 设置饼图的标题
 plt.title(f'Top {top_n} Comment Authors and Others')
+
+# 保存饼图为图片文件，文件名中包含了前N位作者的数量信息
 plt.savefig(f'Top {top_n} Comment Authors and Others')
